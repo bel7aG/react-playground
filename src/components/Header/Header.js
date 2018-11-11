e16b381335913b38a4a49a7d389e74f15f898f8c
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-11 17:01:40 +0100

    organization ..

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <div className="heading">
+      <h1>Header</h1>
+    </div>
+  </header>
+);
+
+export default Header;
